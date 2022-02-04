718c136c99659fde9b905f220b92437a8a066b8f
Myllena-Oliveira <myllenahsoliveira@gmail.com>
2022-02-04 13:36:26 -0300

    adicionando pasta dotNET

---
+++ dotNET/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Treinando.NET
+{
+    class Program
+    {
+            static void Main (string[] args)
+        {
+                int NumeroDeVezes = 5;
+                for (int i = 0; i < NumeroDeVezes; i++)
+                {
+                    Console.WriteLine ($"Treinando .Net {i}");
+                }
+
+        }
+    }
+}
